557d0d414179639131cb9a024f1db618ad305dc8
Haesol Lee <haesol2022@naver.com>
2025-06-04 11:33:20 +0900

    store-pos 일반 폴더 전환

---
+++ store-pos/src/module-info.java
@@ -0,0 +1,9 @@
+module store.pos {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires jasperreports;
+    requires ojdbc14;
+    requires controlsfx;
+
+}
